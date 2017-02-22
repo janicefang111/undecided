5d4871a2749a071252e7a082b3caa859eb5ceaae
janicefang111 <janicefang111@gmail.com>
2017-02-21 21:11:36 -0600

    Create budget.js

---
+++ budget.js
@@ -0,0 +1,28 @@
+for (var i=1; i<5; i++) {
+  var budget = document.getElementById("budget" + i).value;
+  var spent = document.getElementById("spent" + i).innerHTML.replace("$","");
+  var percent = parseFloat(spent)/parseFloat(budget);
+  var percent_filled = document.getElementById("percent_filled" + i);
+  percent_filled.style.width = (percent*60).toFixed(2) + "%";
+};
+
+function functionC(val) {
+  var i = val.id.substring(6);
+  var budget = val.value;
+  var spent = document.getElementById("spent" + i).innerHTML.replace("$","");
+  var percent_filled = document.getElementById("percent_filled" + i);
+  if(parseFloat(budget) == 0) {
+    percent_filled.style.width = "0%";
+    percent_filled.style.background = "#3ba7ff";
+  }
+  if(parseFloat(spent)/parseFloat(budget) > 1) {
+    percent_filled.style.width = "60%";
+    percent_filled.style.background = "red";
+  }
+  else {
+  var percent = parseFloat(spent)/parseFloat(budget);
+  percent_filled.style.width = (percent*60).toFixed(2) + "%";
+  percent_filled.style.background = "#3ba7ff";
+  }
+};
+
