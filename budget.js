cfc6312359f8cf50a13b150c669a508cdcfae930
janicefang111 <janicefang111@gmail.com>
2017-03-06 21:44:09 -0600

    Update budget.js

--- budget.js
+++ budget.js
@@ -4,6 +4,20 @@ for (var i=1; i<5; i++) {
   var percent = parseFloat(spent)/parseFloat(budget);
   var percent_filled = document.getElementById("percent_filled" + i);
   percent_filled.style.width = (percent*59.7).toFixed(2) + "%";
+  var difference = (parseFloat(budget) - parseFloat(spent));
+  var rest = document.getElementById("rest" + i);
+  rest.innerHTML = "$" + Math.abs(difference);
+  var rem_over = document.getElementById("rem_over" + i);
+  if (difference >= 0) {
+    rem_over.innerHTML = "remaining";
+    rem_over.style.color = "black";
+    rest.style.color = "black";
+  }
+  else {
+    rem_over.innerHTML = "over";
+    rem_over.style.color = "#ef4940";
+    rest.style.color = "#ef4940";
+  }
 };
 
 function functionC(val) {
@@ -17,12 +31,26 @@ function functionC(val) {
   }
   if(parseFloat(spent)/parseFloat(budget) > 1) {
     percent_filled.style.width = "59.7%";
-    percent_filled.style.background = "red";
+    percent_filled.style.background = "#ef4940";
   }
   else {
   var percent = parseFloat(spent)/parseFloat(budget);
   percent_filled.style.width = (percent*59.7).toFixed(2) + "%";
   percent_filled.style.background = "#3ba7ff";
+  }
+  var difference = (parseFloat(budget) - parseFloat(spent));
+  var rest = document.getElementById("rest" + i);
+  rest.innerHTML = "$" + Math.abs(difference);
+  var rem_over = document.getElementById("rem_over" + i);
+  if (difference >= 0) {
+    rem_over.innerHTML = "remaining";
+    rem_over.style.color = "black";
+    rest.style.color = "black";
+  }
+  else {
+    rem_over.innerHTML = "over";
+    rem_over.style.color = "#ef4940";
+    rest.style.color = "#ef4940";
   }
 };
 
